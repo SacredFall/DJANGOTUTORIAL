d3ffa2f1f49a93b5d1be05405555a127850de0e3
Eric Chen <ericc404@nycstudents.net>
2025-09-10 11:14:26 -0400

    admin panel

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("You are at the polls index")
 # Create your views here.
+def detail(request, question_id): 
+    return HttpResponse(f"You are looking at question {question_id}.")
+def results(request, question_id):
+    response = f"You are looking at the results of question {question_id}."
+    return HttpResponse(response % question_id)
