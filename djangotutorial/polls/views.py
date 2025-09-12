94575decb8c3bb6ac60561dfce1de248cc8823b5
Eric Chen <ericc404@nycstudents.net>
2025-09-12 11:14:38 -0400

    using templates in my views

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
+from django.http import Http404
 def index(request):
-    latest_question_list = Question.objects.all().order_by("-pub_date")[:5]
-    output = ", ".join([q.question_test for q in latest_question_list])
-    return HttpResponse(f"You are at the polls index. Latest questions are: {output}")
-# Create your views here.
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 def detail(request, question_id): 
     return HttpResponse(f"You are looking at question {question_id}.")
 def results(request, question_id):
-    response = f"You are looking at the results of question {question_id}."
-    return HttpResponse(response)
+    try:
+        question = Question.objects.get(pk=question_id) 
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question" : question})
 def vote(request, question_id):
     return HttpResponse(f"You are voting on question {question_id}.")
